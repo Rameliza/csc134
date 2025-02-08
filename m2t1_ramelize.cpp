57e142e977a9736e394cf35940673069d0bf001f
Rameliza <rameliza1039@student.faytechcc.edu>
2025-02-08 23:34:31 +0000

    m2t1

---
+++ m2t1_ramelize.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M2T1
+// Abreanna Ramelize
+// 2/08/2025
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    cout << "world" <<endl;
+    cout << endl;
+    return 0;
+}
