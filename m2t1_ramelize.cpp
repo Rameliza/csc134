44be31b54e7e692ad5c4641240b513a4f51aa1f7
Rameliza <rameliza1039@student.faytechcc.edu>
2025-02-09 00:15:46 +0000

    M2T1 done

--- m2t1_ramelize.cpp
+++ m2t1_ramelize.cpp
@@ -10,8 +10,28 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello" << endl;
-    cout << "world" <<endl;
+    // variables are like mailboxes 
+    string name = "Abreanna";
+    int num_apples = 10;
+    double price_each = 0.25;
+
+
+    // Declare your variables
+
+    cout << "Welcome to the " << name << " apple farm!" << endl;
+    cout << "We have " << num_apples << " apples in stock." << endl;
+    cout << "Price per apple is: $" << price_each << endl;
+
+    // Calculate total
+    double total_cost = num_apples * price_each;
+
+    // Finally print answer
+    cout << "The total for all " << num_apples;
+    cout <<  " is $" << total_cost << endl;
+
+
     cout << endl;
+
+     
     return 0;
 }
