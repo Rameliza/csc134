162ff89fca108cfb30d1a2e7278e4ca24f8b7189
Rameliza <rameliza1039@student.faytechcc.edu>
2025-02-16 18:50:06 +0000

    M1HW1_ramelize done

---
+++ M1HW1_ramelize.cpp
@@ -0,0 +1,30 @@
+// CSC 134
+// M1HW1
+// Abreanna Ramelize
+// 2/16/2025
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    //setup
+    string movie_name = "Brown Sugar";
+    string director = "Rick Famuyiwa";
+    int year = 2002;
+    double box_office = 28.3;        // million $
+
+    // output
+    cout << "The movie " << movie_name << " came out in " << year << "." << endl;
+    cout << "The director was " << director << "." << endl;
+    cout << "Box office was $" << box_office << " million USD." << endl;
+
+    cout << " When did you fall in love with hip hop scene" << endl;
+    cout << "Syd : Hip hop is my life, and im just trying to keep the romance alive." << endl;
+    cout << "Dre : You can love hip-hop and still have your heart broken." << endl;
+    cout << "Syd : It's like the difference between saying you love somebody and being in love with somebody." << endl;
+    cout << "Dre : You know, i tried taking the path of most resistance and failed miserably. Now im starting to understand what i want: to be a success without selling out." << endl;
+    cout << endl;
+    return 0; // done
+
+}
